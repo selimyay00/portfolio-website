93f1b6fd94bff229e97ab37125884a90b92c7847
selim yay <selimxyay@gmail.com>
2023-12-14 19:58:56 +0300

    optimized the skills page and styling

--- src/components/pages/skills/Skills.jsx
+++ src/components/pages/skills/Skills.jsx
@@ -5,6 +5,11 @@ function Skills() {
         <div className='container'>
             <h2 className='container__title'>Skills</h2>
             <div className='skills-content'>
+                <p className='sub-heading'>
+                    <i>
+                        Frontend Technologies
+                    </i>
+                </p>
                 <p className='container__content'>
                     * HTML
                 </p>
@@ -17,6 +22,27 @@ function Skills() {
                 <p className='container__content'>
                     * React
                 </p>
+                <br />
+                <p className='sub-heading'>
+                    <i>
+                        Development Tools
+                    </i>
+                </p>
+                <p className='container__content'>* VS Code</p>
+                <br />
+                <p className='sub-heading'>
+                    <i>
+                        Design
+                    </i>
+                </p>
+                <p className='container__content'>* Figma</p>
+                <br />
+                <p className='sub-heading'>
+                    <i>
+                        Build Tools
+                    </i>
+                </p>
+                <p className='container__content'>* Vite</p>
             </div>
             
         </div>
